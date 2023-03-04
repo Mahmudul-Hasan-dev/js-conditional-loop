6ac9f76f4156d558de0ce9d5ba7cc1ed00ea5d3c
Mahmudul-Hasan-dev <Contact.mahmudul.h@gmail.com>
2023-03-04 12:38:38 +0600

    first commit

---
+++ while.js
@@ -0,0 +1,9 @@
+var roastGiven = 0
+
+//while loop
+//you have increase variable value otherwise it will be infinite loop
+while (roastGiven < 8) {
+    console.log('here is the roast');
+    console.log(roastGiven);
+    roastGiven++;
+}
